6935b71fa41716e91e666c3e723b9a171975d3be
Izer <izer.onadim19@imperial.ac.uk>
2020-06-13 08:27:07 +0100

    Added append, remove, contains, isEmpty, get, equals and length functions to arrayList

--- arrayList/arrayList.c
+++ arrayList/arrayList.c
@@ -1,21 +1,23 @@
 #include "arrayList.h"
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
-ArrayList_t *createArrayList() {
+ArrayList_t *arrayListCreate(void) {
   
-  ArrayList_t list = (ArrayList_t *) malloc(sizeof(ArrayList_t));
+  ArrayList_t *list = (ArrayList_t *) malloc(sizeof(ArrayList_t));
   if (!list) return NULL;
 
   list->size = 1;
   list->length = 0;
-  list->array = (int *) calloc(1, sizeof(int));
+  list->array = (int *) calloc(list->size, sizeof(int));
   if (!list->array) return NULL;
 
   return list;
 }
 
-void freeArrayList(ArrayList_t *list) {
+void arrayListFree(ArrayList_t *list) {
   
   free(list->array);
   list->array = NULL;
@@ -23,10 +25,73 @@ void freeArrayList(ArrayList_t *list) {
   list = NULL;
 }
 
-bool appendArrayList(ArrayList_t *list, int elem);
+bool arrayListAppend(ArrayList_t *list, int elem) {
+  
+  if (list->length >= list->size) {
+    list->size *= 2;
+    list->array = (int *) realloc(list->array, list->size * sizeof(int));
+    if (!list->array) return false;
+  }
 
-int removeArrayList(ArrayList_t *list, int index);
+  (list->array)[list->length] = elem;
+  list->length++;
 
-bool isEmpty(ArrayList_t *list);
+  return true; 
+}
 
-bool containsArrayList(ArrayList_t *list, int elem);
+int arrayListRemove(ArrayList_t *list, int index) {
+  
+  if (index < 0 || index >= list->length) {
+    fprintf(stderr, "Out of bounds array list access.");
+    exit(EXIT_FAILURE);
+  }
+  
+  int value = (list->array)[index];
+  
+  for (int i = index; i < list->length; i++) {
+    (list->array)[i] = (list->array)[i+1];
+  }
+
+  list->length--;
+  
+  return value;  
+}
+
+bool arrayListIsEmpty(ArrayList_t *list) {
+  return list->length == 0;
+}
+
+int arrayListLength(ArrayList_t *list) {
+  return list->length;
+}
+
+bool arrayListContains(ArrayList_t *list, int elem) {
+  
+  for (int i = 0; i < list->length; i++)
+    if ((list->array)[i] == elem) return true;
+
+  return false;
+}
+
+bool arrayListEquals(ArrayList_t *one, ArrayList_t *two) {
+  
+  if (one->length != two->length) return false;
+
+  for (int i = 0; i < one->length; i++)
+    if ((one->array)[i] != (two->array)[i]) return false;
+  
+  return true;
+}
+
+int arrayListGet(ArrayList_t *list, int index) {
+  
+  if (index < 0 || index >= list->length) {
+    fprintf(stderr, "Out of bounds array list access.");
+    exit(EXIT_FAILURE);
+  }
+  
+  return (list->array)[index];
+}
+
+bool arrayListInsert(ArrayList_t *list, int index, int elem);  
+
--- arrayList/arrayList.h
+++ arrayList/arrayList.h
@@ -13,33 +13,54 @@ typedef struct ArrayList {
  * Creates an array list with dynamically allocated memory and 
  * returns a pointer to it. Returns NULL if allocation fails.
  */
-ArrayList_t *createArrayList();
+ArrayList_t *arrayListCreate(void);
 
 /*
  * Frees all memory allocated to given array list.
  */
-void freeArrayList(ArrayList_t *list);
+void arrayListFree(ArrayList_t *list);
 
 /*
  * Appends new element to the end of given array list.
  * Returns true if successful.
  */
-bool appendArrayList(ArrayList_t *list, int elem);
+bool arrayListAppend(ArrayList_t *list, int elem);
 
 /*
  * Removes an element from the a given location in the list 
  * and returns it. 
  */
-int removeArrayList(ArrayList_t *list, int index);
+int arrayListRemove(ArrayList_t *list, int index);
 
 /*
  * Returns true if there are no elements in the given list.
  */
-bool isEmpty(ArrayList_t *list);
+bool arrayListIsEmpty(ArrayList_t *list);
+
+/*
+ * Returns the length of the array list;
+ */
+int arrayListLength(ArrayList_t *list);
 
 /*
  * Returns true if given list contains given element.
  */
-bool containsArrayList(ArrayList_t *list, int elem);
+bool arrayListContains(ArrayList_t *list, int elem);
+
+/*
+ * Returns true if the two given arraylists are equal.
+ */
+bool arrayListEquals(ArrayList_t *one, ArrayList_t *two); 
+
+/*
+ * Returns element at given index, gives out of bounds error.
+ */
+int arrayListGet(ArrayList_t *list, int index); 
+
+/*
+ * Inserts element at given index in given list, 
+ * returns false if realloc fails of element not inserted.
+ */
+bool arrayListInsert(ArrayList_t *list, int index, int elem); 
 
 #endif
