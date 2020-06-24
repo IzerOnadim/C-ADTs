f56a1a29b7f7bb9de5a0411dee17ba25ec7e1bc9
Izer <izer.onadim19@imperial.ac.uk>
2020-06-24 16:50:32 +0100

    Test appendAllArrayList function & add line no. to test fail message

--- arrayList/testArrayList.c
+++ arrayList/testArrayList.c
@@ -5,24 +5,37 @@
 
 #include "arrayList.h"
 
+#define MAX_LINE_LENGTH (50)
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+#define testTrue(a) testTrueLine(a, __LINE__)
+#define testEquals(a, b) testEqualsLine(a, b,__LINE__)
 
 int NUM_TESTS = 0;
 int TESTS_FAILED = 0;
 
-bool testTrue(bool condition, char *testname) {
+bool testTrueLine(bool condition, int line) {
   NUM_TESTS++;
+  char fail[MAX_LINE_LENGTH];
+  sprintf(fail, ANSI_COLOR_RED "FAIL - line: %d" ANSI_COLOR_RESET, line);
   if (!condition) TESTS_FAILED++;
-  printf("%s: %s\n", testname, condition ?  
-      ANSI_COLOR_GREEN "OK"  ANSI_COLOR_RESET :
-      ANSI_COLOR_RED  "FAIL" ANSI_COLOR_RESET);
+  printf("Test %2d: %s\n", NUM_TESTS, condition ?  
+      ANSI_COLOR_GREEN "PASS"  ANSI_COLOR_RESET : fail);
  return condition; 
 }
 
-bool testEquals(int one, int two, char *testname) {
- return testTrue(one == two, testname); 
+bool testEqualsLine(int one, int two, int line) {
+ return testTrueLine(one == two, line); 
+}
+
+void testToString(ArrayList_t *list, char *expected) {
+  char *actual = arrayListToString(list);
+  if (!testTrue(!strcmp(actual, expected))) {
+    printf("Expected: %s\n", expected);
+    printf("Actual:   %s\n", actual); 
+  }
+  free(actual);
 }
 
 bool equalArrays(int *one, int *two, int size) {
@@ -49,193 +62,185 @@ int main(void) {
   appendArrayList(list, 1);
   appendArrayList(list, 2);
   appendArrayList(list, 3);
-  testTrue(containsArrayList(list, 3), "Test 01");
-  testTrue(containsArrayList(list, 2), "Test 02");
-  testTrue(containsArrayList(list, 0), "Test 03");
-  testTrue(containsArrayList(list, 1), "Test 04");
+  testTrue(containsArrayList(list, 3));
+  testTrue(containsArrayList(list, 2));
+  testTrue(containsArrayList(list, 0));
+  testTrue(containsArrayList(list, 1));
 
   printf("\n---------removeArrayList & containsArrayList tests----\n\n");
   removeArrayList(list, 0);
-  testTrue(!containsArrayList(list, 0), "Test 05");
+  testTrue(!containsArrayList(list, 0));
   removeArrayList(list, 0);
-  testTrue(!containsArrayList(list, 1), "Test 06");
+  testTrue(!containsArrayList(list, 1));
   removeArrayList(list, 0);
-  testTrue(!containsArrayList(list, 2), "Test 07");
+  testTrue(!containsArrayList(list, 2));
   removeArrayList(list, 0);
-  testTrue(!containsArrayList(list, 3), "Test 08");
+  testTrue(!containsArrayList(list, 3));
   appendArrayList(list, 0);
   appendArrayList(list, 0);
   removeArrayList(list, 1);
-  testTrue(containsArrayList(list, 0), "Test 09");
+  testTrue(containsArrayList(list, 0));
   removeArrayList(list, 0);
-  testTrue(!containsArrayList(list, 0), "Test 10");
+  testTrue(!containsArrayList(list, 0));
   
   printf("\n---------isEmptyArrayList tests----------------------\n\n");
   appendArrayList(list, 13);
-  testTrue(!isEmptyArrayList(list), "Test 11");
+  testTrue(!isEmptyArrayList(list));
   removeArrayList(list, 0);
-  testTrue(isEmptyArrayList(list), "Test 12");
+  testTrue(isEmptyArrayList(list));
   
   printf("\n---------lengthArrayList tests------------------------\n\n");
   appendArrayList(list, 13);
-  testEquals(lengthArrayList(list), 1, "Test 13");
+  testEquals(lengthArrayList(list), 1);
   appendArrayList(list, 0);
-  testEquals(lengthArrayList(list), 2, "Test 14");
+  testEquals(lengthArrayList(list), 2);
   appendArrayList(list, 2753);
-  testEquals(lengthArrayList(list), 3, "Test 15");
+  testEquals(lengthArrayList(list), 3);
   appendArrayList(list, 123456);
-  testEquals(lengthArrayList(list), 4, "Test 16");
+  testEquals(lengthArrayList(list), 4);
   removeArrayList(list, 3);
-  testEquals(lengthArrayList(list), 3, "Test 17");
+  testEquals(lengthArrayList(list), 3);
   removeArrayList(list, 2);
-  testEquals(lengthArrayList(list), 2, "Test 18");
+  testEquals(lengthArrayList(list), 2);
   removeArrayList(list, 1);
-  testEquals(lengthArrayList(list), 1, "Test 19");
+  testEquals(lengthArrayList(list), 1);
   removeArrayList(list, 0);
-  testEquals(lengthArrayList(list), 0, "Test 20");
+  testEquals(lengthArrayList(list), 0);
   
   printf("\n---------equalsArrayList tests------------------------\n\n");
   ArrayList_t *other = createArrayList();
-  testTrue(equalsArrayList(list, other), "Test 21");
+  testTrue(equalsArrayList(list, other));
   appendArrayList(other, 1); 
-  testTrue(!equalsArrayList(list, other), "Test 22");
+  testTrue(!equalsArrayList(list, other));
   appendArrayList(other, 2); 
   appendArrayList(list, 1); 
   appendArrayList(list, 2); 
-  testTrue(equalsArrayList(list, other), "Test 23");
+  testTrue(equalsArrayList(list, other));
   appendArrayList(list, 2); 
-  testTrue(!equalsArrayList(list, other), "Test 24");
+  testTrue(!equalsArrayList(list, other));
   appendArrayList(other, 2); 
-  testTrue(equalsArrayList(list, other), "Test 25");
+  testTrue(equalsArrayList(list, other));
   removeArrayList(list, 2);
-  testTrue(!equalsArrayList(list, other), "Test 26");
+  testTrue(!equalsArrayList(list, other));
   removeArrayList(other, 1);
-  testTrue(equalsArrayList(list, other), "Test 27");
+  testTrue(equalsArrayList(list, other));
   removeArrayList(other, 0);
   removeArrayList(other, 0);
   removeArrayList(list, 0);
   removeArrayList(list, 0);
-  testTrue(equalsArrayList(list, other), "Test 28");
+  testTrue(equalsArrayList(list, other));
   
   printf("\n---------getArrayList tests--------------------------\n\n");
   appendArrayList(list, 13);
-  testEquals(getArrayList(list, 0), 13, "Test 29");
-  testEquals(getArrayList(list, 0), 13, "Test 30");
+  testEquals(getArrayList(list, 0), 13);
+  testEquals(getArrayList(list, 0), 13);
   appendArrayList(list, 246772);
   appendArrayList(list, -5463);
   appendArrayList(list, 0x500);
   appendArrayList(list, -0x500);
-  testEquals(getArrayList(list, 1), 246772, "Test 31");
-  testEquals(getArrayList(list, 2), -5463, "Test 32");
-  testEquals(getArrayList(list, 3), 0x500, "Test 33");
-  testEquals(getArrayList(list, 4), -0x500, "Test 34");
+  testEquals(getArrayList(list, 1), 246772);
+  testEquals(getArrayList(list, 2), -5463);
+  testEquals(getArrayList(list, 3), 0x500);
+  testEquals(getArrayList(list, 4), -0x500);
 
   printf("\n---------clearArrayList tests------------------------\n\n");
-  testTrue(!isEmptyArrayList(list), "Test 35");
+  testTrue(!isEmptyArrayList(list));
   clearArrayList(list);
-  testTrue(isEmptyArrayList(list), "Test 36");
-  testTrue(!containsArrayList(list, 13), "Test 37");
-  testTrue(!containsArrayList(list, 246772), "Test 38");
+  testTrue(isEmptyArrayList(list));
+  testTrue(!containsArrayList(list, 13));
+  testTrue(!containsArrayList(list, 246772));
 
   printf("\n---------insertArrayList tests-----------------------\n\n");
   appendArrayList(list, 0);  
   appendArrayList(list, 4);  
   insertArrayList(list, 1, 2);
-  testTrue(containsArrayList(list, 2), "Test 39");
-  testEquals(getArrayList(list, 1), 2, "Test 40");
-  testEquals(removeArrayList(list, 0), 0, "Test 41");
-  testEquals(removeArrayList(list, 0), 2, "Test 42");
-  testEquals(removeArrayList(list, 0), 4, "Test 43");
-  testTrue(isEmptyArrayList(list), "Test 44");
+  testTrue(containsArrayList(list, 2));
+  testEquals(getArrayList(list, 1), 2);
+  testToString(list, "[0, 2, 4]");
+  testEquals(removeArrayList(list, 0), 0);
+  testEquals(removeArrayList(list, 0), 2);
+  testEquals(removeArrayList(list, 0), 4);
+  testTrue(isEmptyArrayList(list));
   insertArrayList(list, 0, 3);
-  testTrue(containsArrayList(list, 3), "Test 45");
-  testEquals(removeArrayList(list, 0), 3, "Test 46");
+  testTrue(containsArrayList(list, 3));
+  testEquals(removeArrayList(list, 0), 3);
 
   printf("\n---------indexOfArrayList tests----------------------\n\n");
   appendArrayList(list, 0);
   appendArrayList(list, 1);
   appendArrayList(list, 2);
-  testEquals(indexOfArrayList(list, 0), 0, "Test 47"); 
-  testEquals(indexOfArrayList(list, 1), 1, "Test 48"); 
-  testEquals(indexOfArrayList(list, 2), 2, "Test 49"); 
-  testEquals(indexOfArrayList(list, 9), -1, "Test 50"); 
-  testEquals(indexOfArrayList(list, 8), -1, "Test 51"); 
+  testEquals(indexOfArrayList(list, 0), 0); 
+  testEquals(indexOfArrayList(list, 1), 1); 
+  testEquals(indexOfArrayList(list, 2), 2); 
+  testEquals(indexOfArrayList(list, 9), -1); 
+  testEquals(indexOfArrayList(list, 8), -1); 
   clearArrayList(list); 
-  testEquals(indexOfArrayList(list, 0), -1, "Test 52");
+  testEquals(indexOfArrayList(list, 0), -1);
 
   printf("\n---------initArrayList tests-------------------------\n\n");
   int arr[] = {1, 2, 3, 4, 5};
   ArrayList_t *another = initArrayList(arr, 5);
-  testTrue(containsArrayList(another, 1), "Test 53");
-  testTrue(containsArrayList(another, 2), "Test 54");
-  testTrue(containsArrayList(another, 3), "Test 55");
-  testTrue(containsArrayList(another, 4), "Test 56");
-  testTrue(containsArrayList(another, 5), "Test 57");
-  testTrue(!containsArrayList(another, 0), "Test 58");
-  testTrue(!containsArrayList(another, 6), "Test 59");
-  testTrue(!containsArrayList(another, 8), "Test 60");
+  testTrue(containsArrayList(another, 1));
+  testTrue(containsArrayList(another, 2));
+  testTrue(containsArrayList(another, 3));
+  testTrue(containsArrayList(another, 4));
+  testTrue(containsArrayList(another, 5));
+  testTrue(!containsArrayList(another, 0));
+  testTrue(!containsArrayList(another, 6));
+  testTrue(!containsArrayList(another, 8));
 
   printf("\n---------arrayListToArray tests----------------------\n\n");
   int *newArr = arrayListToArray(another);
-  testTrue(equalArrays(arr, newArr, another->length), "Test 61");
+  testTrue(equalArrays(arr, newArr, another->length));
   int arr1[1];
   int *arr2 = arrayListToArray(list);
-  testTrue(equalArrays(arr1, arr2, list->length), "Test 62");
+  testTrue(equalArrays(arr1, arr2, list->length));
 
   printf("\n---------arrayListToString tests---------------------\n\n");
-  char expectedStr[] = "[1, 2, 3, 4, 5]";
-  char *strTest = arrayListToString(another);
-  if (!testTrue(!strcmp(strTest, expectedStr), "Test 63")) {
-    printf("Expected: %s\n", expectedStr);
-    printf("Actual:   %s\n", strTest); 
-  }
-
+  testToString(another, "[1, 2, 3, 4, 5]");
+  appendArrayList(another, 6);
+  testToString(another, "[1, 2, 3, 4, 5, 6]");
+  insertArrayList(another, 0, 0);
+  testToString(another, "[0, 1, 2, 3, 4, 5, 6]");
   clearArrayList(another);
-  char expected[] = "[]";
-  char *actual = arrayListToString(another);
-  if (!testTrue(!strncmp(actual, expected, 3), "Test 64")) {
-    printf("Expected: %s\n", expected);
-    printf("Actual:   %s\n", actual); 
-  }
+  testToString(another, "[]");
 
   printf("\n---------cloneArrayList tests------------------------\n\n");
   ArrayList_t *clonedList = cloneArrayList(list);
-  testTrue(clonedList != list, "Test 65"); 
-  testTrue(equalsArrayList(list, clonedList), "Test 66");
+  testTrue(clonedList != list); 
+  testTrue(equalsArrayList(list, clonedList));
   appendArrayList(list, 1); 
   appendArrayList(list, 2); 
   appendArrayList(list, 3); 
   appendArrayList(list, 4); 
   ArrayList_t *clonedList1 = cloneArrayList(list);
-  testTrue(clonedList1 != list, "Test 67"); 
-  testTrue(equalsArrayList(list, clonedList1), "Test 68");
-  testTrue(containsArrayList(clonedList1, 1), "Test 69");
-  testTrue(containsArrayList(clonedList1, 2), "Test 70");
-  testTrue(containsArrayList(clonedList1, 3), "Test 71");
-  testTrue(containsArrayList(clonedList1, 4), "Test 72");
-  testTrue(!containsArrayList(clonedList1, 0), "Test 73");
-  testTrue(!containsArrayList(clonedList1, 5), "Test 74");
-  testEquals(lengthArrayList(clonedList1), 4, "Test 75");
-  char expected1[] = "[1, 2, 3, 4]";
-  char *actual1 = arrayListToString(clonedList1);
-  if (!testTrue(!strcmp(actual1, expected1), "Test 76")) {
-    printf("Expected: %s\n", expected1);
-    printf("Actual:   %s\n", actual1); 
-  }
+  testTrue(clonedList1 != list); 
+  testTrue(equalsArrayList(list, clonedList1));
+  testTrue(containsArrayList(clonedList1, 1));
+  testTrue(containsArrayList(clonedList1, 2));
+  testTrue(containsArrayList(clonedList1, 3));
+  testTrue(containsArrayList(clonedList1, 4));
+  testTrue(!containsArrayList(clonedList1, 0));
+  testTrue(!containsArrayList(clonedList1, 5));
+  testEquals(lengthArrayList(clonedList1), 4);
+  testToString(clonedList1, "[1, 2, 3, 4]");
 
-  printf("\n---------cloneArrayList tests------------------------\n\n");
+  printf("\n---------subListArrayList tests----------------------\n\n");
   ArrayList_t *subList = subListArrayList(list, 1, 3); 
-  testEquals(lengthArrayList(subList), 2, "Test 77");
-  testTrue(containsArrayList(subList, 2), "Test 78");
-  testTrue(containsArrayList(subList, 3), "Test 79");
-  testTrue(!containsArrayList(subList, 1), "Test 80");
-  testTrue(!containsArrayList(subList, 4), "Test 81");
-  char expected2[] = "[2, 3]";
-  char *actual2 = arrayListToString(subList);
-  if (!testTrue(!strcmp(actual2, expected2), "Test 82")) {
-    printf("Expected: %s\n", expected2);
-    printf("Actual:   %s\n", actual2); 
-  }
+  testEquals(lengthArrayList(subList), 2);
+  testTrue(containsArrayList(subList, 2));
+  testTrue(containsArrayList(subList, 3));
+  testTrue(!containsArrayList(subList, 1));
+  testTrue(!containsArrayList(subList, 4));
+  testToString(subList, "[2, 3]");
+
+  printf("\n---------appendAllArrayList tests--------------------\n\n");
+  appendAllArrayList(other, subList);
+  testTrue(!isEmptyArrayList(other));
+  testTrue(containsArrayList(other, 2));
+  testTrue(containsArrayList(other, 3));
+  testTrue(!containsArrayList(other, 1));
+  testToString(other, "[2, 3]");
 
   printf("\n-----------------------SUMMARY-----------------------\n");
   printf("\nNo. Tests: %d\nPassed:    %d\nFailed:    %d\n", 
@@ -249,9 +254,5 @@ int main(void) {
   freeArrayList(subList);
   free(newArr);
   free(arr2); 
-  free(strTest);
-  free(actual);
-  free(actual1);
-  free(actual2);
   printf("\n------------------------------------------------------\n\n");
 }
